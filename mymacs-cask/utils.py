0226ac4a7a6c18800c5afcc5e3f63e1355205f38
wwq0327 <wwq0327@gmail.com>
2014-05-08 14:01:44 +0800

    终端颜色

--- mymacs-cask/utils.py
+++ mymacs-cask/utils.py
@@ -5,6 +5,18 @@ import sys
 import os
 import zipfile
 
+## style dict
+STYLE = {
+	"clear"  : "\033[0m",
+	"red"    : "\033[31m",
+	"green"  : "\033[32m",
+	"yellow" : "\033[33m",
+	"blue"   : "\033[34m",
+	"purple" : "\033[35m",
+	"cyan"   : "\033[36m",
+	"white"  : "\033[37m",
+	}
+	
 def extract_zip(zfile, path):
 	if not zipfile.is_zipfile(zfile):
 		print "%s 不是有效的zip文件，或文件已坏损" % zfile
@@ -14,9 +26,19 @@ def extract_zip(zfile, path):
 	z.extractall(path)
 	print "文件解压到:%s" % path
 	z.close()
+
+def highlight(style, text):
+	"""打印彩色的终端颜色
+	"""
+	k = style.lower()
+	if k not in STYLE.keys():
+		return text
+	return ("%("+k+")s" + text + "%(clear)s") % STYLE
 	
 def test():
-	extract_zip("../testdatas/test.zip", "/tmp")
+	#extract_zip("../testdatas/test.zip", "/tmp")
+	for k in STYLE.keys():
+		print highlight(k, "hello, world") + " => " + k
 
 if __name__ == '__main__':
 	test()
